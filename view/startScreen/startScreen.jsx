b42801ffd7067fd0655ca1643dd46c1eee8d2f97
Jesper Jonsson <jesper.m.jonsson@gmail.com>
2024-04-04 12:02:15 +0200

    Center screen content

--- view/startScreen/startScreen.jsx
+++ view/startScreen/startScreen.jsx
@@ -14,17 +14,30 @@ const App = () => {
   }, []);
 
   return (
-    <div className="bam-container" data-padding="normal">
+    <div className="bam-container" data-padding="normal" style={style.root}>
       <div className="bam-container" data-spacing="x-small">
         <div className="bam-headline1">Welcome  👋</div>
         <div className="bam-title">A sandbox for the Bambuser App Framework</div>
       </div>
-      <div className="bam-container">
+      <div className="bam-container" style={style.content}>
         <p>Edit <b>view/startScreen/startScreen.jsx</b> and remove the disabled attribute on "Continue" button below 👷‍♀️</p>
         <button data-variant="primary" onClick={onClickContinue} disabled>Continue 🚀</button>
       </div>
     </div>
   )
+};
+
+const style = {
+  root: {
+    height: '100vh',
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    textAlign: 'center',
+  },
+  content: {
+    alignItems: 'center',
+  },
 };
 
 ReactDOM.createRoot(document.getElementById('root')).render(
