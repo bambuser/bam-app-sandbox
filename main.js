14f644b9f3d02a91dc62be6c0f830d62c15a6f0f
Jesper Jonsson <jesper.m.jonsson@gmail.com>
2024-04-04 10:14:20 +0200

    Remove example console logs

--- main.js
+++ main.js
@@ -1,21 +1,5 @@
 const appContext = await bambuserAppFramework.getContext();
 
-//
-// TODO: Replace with your main app logic
-//
-console.log(`%cBambuser App Framework%c
-The example app is alive!
-
-%c🔧 Use the bam-app.config.js file to configure your app manifest (ex. ading additional permissions to gain access to APIs)
-🤓 Available API in main app context: %o'
-👷‍♀️ Example of composing url to a custom view (see docs for ex. screens): %s\n\n`,
-'font-size: 3em; padding-top: 1em; padding-bottom: 0.5em',
-'font-size: 1.2em; font-weight: bold;',
-'font-size: 1.2em; padding-bottom: 0.2em',
-appContext,
-new URL('./view/startScreen/startScreen.html', new URL(import.meta.url)).href,
-);
-
 const { screenApi } = appContext;
 if (screenApi) {
   screenApi.on('provide-instance', async ({ id }) => {
