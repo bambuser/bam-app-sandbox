9d7e00011aecd764c4939f2474fc760f0b6df346
Jesper Jonsson <jesper.m.jonsson@gmail.com>
2024-04-04 13:42:30 +0200

    Use better example text

--- view/startScreen/startScreen.jsx
+++ view/startScreen/startScreen.jsx
@@ -5,6 +5,10 @@ const appContext = await bambuserAppFramework.getContext();
 const { screen } = appContext;
 
 const App = () => {
+  const onClickContinue = () => {
+    screen.close({ action: 'next' });
+  };
+
   useEffect(() => {
     screen.setReady();
   }, []);
@@ -12,12 +16,12 @@ const App = () => {
   return (
     <div className="bam-container" data-padding="normal">
       <div className="bam-container" data-spacing="x-small">
-        <div className="bam-headline1">Welcome</div>
-        <div className="bam-title">A Bambuser App Framework custom view</div>
+        <div className="bam-headline1">Welcome  👋</div>
+        <div className="bam-title">A sandbox for the Bambuser App Framework</div>
       </div>
       <div className="bam-container">
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla sit amet enim feugiat, blandit mi in, volutpat tellus. Phasellus eget erat at nibh pulvinar interdum. Vivamus imperdiet pharetra nulla, ac imperdiet nisl finibus at. Praesent cursus elementum viverra. Duis tempus erat risus, at auctor quam malesuada vel. Praesent sit amet magna at massa finibus aliquam.</p>
-        <button data-variant="primary">Click click click</button>
+        <p>Edit <b>view/startScreen/startScreen.jsx</b> and remove the disabled attribute on "Continue" button below 👷‍♀️</p>
+        <button data-variant="primary" onClick={onClickContinue} disabled>Continue 🚀</button>
       </div>
     </div>
   )
