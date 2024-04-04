7a0d07887d98dc2b149a0ab94da1a37f5445585d
Jesper Jonsson <jesper.m.jonsson@gmail.com>
2024-04-04 13:35:28 +0200

    Add logo to screen

--- view/startScreen/startScreen.jsx
+++ view/startScreen/startScreen.jsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 
 const appContext = await bambuserAppFramework.getContext();
 const { screen } = appContext;
+const logoUrl = new URL('../../assets/logo.svg', import.meta.url).href;
 
 const App = () => {
   const onClickContinue = () => {
@@ -15,14 +16,17 @@ const App = () => {
 
   return (
     <div className="bam-container" data-padding="normal" style={style.root}>
-      <div className="bam-container" data-spacing="x-small">
-        <div className="bam-headline1">Welcome  👋</div>
-        <div className="bam-title">A sandbox for the Bambuser App Framework</div>
+      <div className="bam-container">
+        <div className="bam-container" data-spacing="x-small">
+          <div className="bam-headline1">Welcome  👋</div>
+          <div className="bam-title">A sandbox for the Bambuser App Framework</div>
+        </div>
+        <div className="bam-container" style={style.content}>
+          <p>Edit <b>view/startScreen/startScreen.jsx</b> and remove the disabled attribute on "Continue" button below 👷‍♀️</p>
+          <button data-variant="primary" onClick={onClickContinue} disabled>Continue 🚀</button>
+        </div>
       </div>
-      <div className="bam-container" style={style.content}>
-        <p>Edit <b>view/startScreen/startScreen.jsx</b> and remove the disabled attribute on "Continue" button below 👷‍♀️</p>
-        <button data-variant="primary" onClick={onClickContinue} disabled>Continue 🚀</button>
-      </div>
+      <img src={logoUrl} alt="Bambuser logo" style={style.logo} />
     </div>
   )
 };
@@ -31,13 +35,16 @@ const style = {
   root: {
     height: '100vh',
     display: 'flex',
-    justifyContent: 'center',
+    justifyContent: 'space-evenly',
     alignItems: 'center',
     textAlign: 'center',
   },
   content: {
     alignItems: 'center',
   },
+  logo: {
+    width: 140,
+  }
 };
 
 ReactDOM.createRoot(document.getElementById('root')).render(
